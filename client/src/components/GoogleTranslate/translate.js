54867c74653600e0fbee1bbff8c09bb0c0c9de76
Mike Zanercik <Michael.zanercik@gmail.com>
2020-01-09 12:59:35 -0500

    translation

---
+++ client/src/components/GoogleTranslate/translate.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+function Translate() {
+    return (
+        <div className="row" id="translate">
+            <div id="google_translate_element"></div>
+        </div>
+    )
+}
+
+export default Translate;
--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import "./home.css"
 
+import Translate from '../../components/GoogleTranslate/translate'
+
 
 class HomePage extends Component {
   render() {
@@ -12,6 +14,11 @@ class HomePage extends Component {
             <h1>Striker Soccer Center of Huntersville</h1>
             <p>Check out games taking place or reserve a field for you and your team.</p>
             </div>
+          </div>
+        </div>
+        <div className='row' id="translate">
+          <div className='col'>
+            <Translate />
           </div>
         </div>
       </div>
